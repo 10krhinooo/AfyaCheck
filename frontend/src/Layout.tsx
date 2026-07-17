56f4cda341bd1f837454257a5e7dc8422ca2635b
10krhinoo <vkimanga@gmail.com>
2026-07-17 20:22:01 +0300

    Add i18n framework with en/sw locales and nav language switcher

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,18 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import { AuthProvider } from './lib/auth/AuthContext'
+import { I18nProvider } from './lib/i18n'
 import { NavBar } from './components/NavBar'
 import { Footer } from './components/Footer'
 
 export default function Layout() {
   return (
-    <AuthProvider>
-      <div className="flex min-h-screen flex-col">
-        <NavBar />
-        <div className="flex-1">
-          <Outlet />
+    <I18nProvider>
+      <AuthProvider>
+        <div className="flex min-h-screen flex-col">
+          <NavBar />
+          <div className="flex-1">
+            <Outlet />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </AuthProvider>
+      </AuthProvider>
+    </I18nProvider>
   )
 }
